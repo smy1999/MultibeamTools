71fdffa0edf92261648674da286a810d0586bf6d
86598 <865988027@qq.com>
2021-08-03 02:45:10 +0800

    test vpn

--- Demo/mblist_file_to_xlsx/mblist_file_to_xlsx.py
+++ Demo/mblist_file_to_xlsx/mblist_file_to_xlsx.py
@@ -2,7 +2,7 @@ from ConversionToolbox import MBlist as ml
 
 
 def test():
-    file = '../mblist_file_to_xlsx/mblist_data'
+    file = 'D:/Study/mbsystem/mblist/testcon'
     comm = 'mblist -I0158_20160830_025854_EX1607_MB.all.mb58 -OXYTBGgV#.A.d -MA -X testcon -G*'
     SDT = ml.SwathDataTable(file, comm)
     SDT.write_xls(file_path='../mblist_file_to_xlsx/')
--- Test.py
+++ Test.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from ConversionToolbox import AmplitudeGrazingAngle as AGA
 from CurveFittingToolbox import GSAB as cf
+import numpy as np
+import math
+from matplotlib import pyplot as plt
 import time
-
-
-def test_import():
-    file_path2 = 'C:/Users/86598/Downloads/0158_20160830_025854_EX1607_MB.all.mb58.sga'
-    file_path3 = 'C:/Users/86598/Downloads/0158_20160830_025854_EX1607_MB.all.mb58.aga'
-    c = AGA.AGATable(file_path3)
 
 
 def test_rdxls():
@@ -17,39 +14,6 @@ def test_rdxls():
     data = pd.read_excel(file, sheet_name='Sheet1', engine='openpyxl')
     print(data)
     print(type(data))
-
-
-def test_unit():
-    sup = {'A': -30,
-           'B': 0,
-           'C': -55,
-           'D': 0,
-           'E': -55,
-           'F': 0}
-    inf = {'A': -10,
-           'B': 10,
-           'C': -15,
-           'D': 2.5,
-           'E': -20,
-           'F': 40}
-    cf.standard2original(sup)
-    cf.standard2original(inf)
-    list1 = []
-    list2 = []
-    for i, key in enumerate(['A', 'B', 'C', 'D', 'E', 'F']):
-        list1.append(sup[key])
-        list2.append(inf[key])
-
-    print(list1)
-    print(list2)
-
-
-def test_write_img():
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    image = np.random.randn(100, 100)
-    plt.imsave('new_1.png', image)
 
 
 def test_new_curve():
@@ -109,28 +73,195 @@ def test_time():
     print(time2)
 
 
-def test_dtype():
-    import numpy as np
-    a = ['a', 1, 0.5]
-    x = np.array(a)
-    x[1] = x[1].astype(int)
-    print(type(x[1]))
-    print(x)
+def test_softmax():
+    x = [12, 14]
+    print(softmax(x))
 
 
-def test_split():
-    path = 'asdf/asdfee/asdfasdf'
-    path = path.split('/')
-    print(path)
-    print(path[-1])
+def softmax(x):
+    y = np.power(np.e, x)
+    s = y.sum()
+    y = y / y.sum()
+    return y
 
 
-def test():
-    dir = {1:'5',2:'6',3:'7'}
-    list = [1,2,3]
-    print([dir[key] for key in list])
+def test_pe():
+    print(test_position_encoding(4, 0))
+    print(test_position_encoding(4, 1))
+    print(test_position_encoding(4, 2))
+    print(test_position_encoding(4, 3))
 
+
+def test_position_encoding(dimension, t):
+    pos = []
+    for k in range(int(dimension / 2)):
+        w = 1 / np.power(10000, 2 * k / dimension)
+        pos.append(np.sin(w * t))
+        pos.append(np.cos(w * t))
+    return pos
+
+
+def test_binary():
+    n = 10
+    p = 0.57
+    ans = []
+    for i in range(n + 1):
+        ans.append(binary(n, p, i))
+    plt.bar(range(n + 1), ans)
+    plt.show()
+
+
+def binary(n, p, k):
+    coefficient = math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+    ans = coefficient * math.pow(p, k) * math.pow(1 - p, n - k)
+    return ans
+
+
+def test_transformer_position_encoding():
+    import seaborn as sns
+    import math
+
+    def get_positional_encoding(max_seq_len, embed_dim):
+        # 初始化一个positional encoding
+        # embed_dim: 字嵌入的维度
+        # max_seq_len: 最大的序列长度
+        positional_encoding = \
+            np.array(
+                [
+            [pos / np.power(10000, 2 * i / embed_dim) for i in range(embed_dim)]
+            if pos != 0 else np.zeros(embed_dim)
+                    for pos in range(max_seq_len)
+                ]
+            )
+
+        # print(positional_encoding)
+        # print('max_seq_len =', max_seq_len)
+        # print('embed_dim =', embed_dim)
+        # print(positional_encoding.shape)
+
+        positional_encoding[1:, 0::2] = np.sin(positional_encoding[1:, 0::2])  # dim 2i 偶数
+        positional_encoding[1:, 1::2] = np.cos(positional_encoding[1:, 1::2])  # dim 2i+1 奇数
+        # 归一化, 用位置嵌入的每一行除以它的模长
+        # denominator = np.sqrt(np.sum(position_enc**2, axis=1, keepdims=True))
+        # position_enc = position_enc / (denominator + 1e-8)
+        return positional_encoding
+
+    positional_encoding = get_positional_encoding(max_seq_len=100, embed_dim=16)
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(positional_encoding)
+    plt.title("Sinusoidal Function")
+    plt.xlabel("hidden dimension")
+    plt.ylabel("sequence length")
+    plt.show()
 
 
 if __name__ == '__main__':
-    test()
+    test_transformer_position_encoding()
+
+
+
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+        # self.temperature是论文中的d_k ** 0.5，防止梯度过大
+        # QxK/sqrt(dk)
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+
+        if mask is not None:
+            # 屏蔽不想要的输出
+            attn = attn.masked_fill(mask == 0, -1e9)
+        # softmax+dropout
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        # 概率分布xV
+        output = torch.matmul(attn, v)
+
+        return output, attn
+
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    # n_head头的个数，默认是8
+    # d_model编码向量长度，例如本文说的512
+    # d_k, d_v的值一般会设置为 n_head * d_k=d_model，
+    # 此时concat后正好和原始输入一样，当然不相同也可以，因为后面有fc层
+    # 相当于将可学习矩阵分成独立的n_head份
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+        # 假设n_head=8，d_k=64
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+        # d_model输入向量，n_head * d_k输出向量
+        # 可学习W^Q，W^K,W^V矩阵参数初始化
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        # 最后的输出维度变换操作
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+        # 单头自注意力
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+        self.dropout = nn.Dropout(dropout)
+        # 层归一化
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+    def forward(self, q, k, v, mask=None):
+        # 假设qkv输入是(b,100,512),100是训练每个样本最大单词个数
+        # 一般qkv相等，即自注意力
+        residual = q
+        # 将输入x和可学习矩阵相乘，得到(b,100,512)输出
+        # 其中512的含义其实是8x64，8个head，每个head的可学习矩阵为64维度
+        # q的输出是(b,100,8,64),kv也是一样
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # 变成(b,8,100,64)，方便后面计算，也就是8个头单独计算
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)   # For head axis broadcasting.
+        # 输出q是(b,8,100,64),维持不变,内部计算流程是：
+        # q*k转置，除以d_k ** 0.5，输出维度是b,8,100,100即单词和单词直接的相似性
+        # 对最后一个维度进行softmax操作得到b,8,100,100
+        # 最后乘上V，得到b,8,100,64输出
+        q, attn = self.attention(q, k, v, mask=mask)
+
+        # b,100,8,64-->b,100,512
+        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        q = self.dropout(self.fc(q))
+        # 残差计算
+        q += residual
+        # 层归一化，在512维度计算均值和方差，进行层归一化
+        q = self.layer_norm(q)
+
+        return q, attn
+
+
+class PositionwiseFeedForward(nn.Module):
+    ''' A two-feed-forward-layer module '''
+
+    def __init__(self, d_in, d_hid, dropout=0.1):
+        super().__init__()
+        # 两个fc层，对最后的512维度进行变换
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        residual = x
+
+        x = self.w_2(F.relu(self.w_1(x)))
+        x = self.dropout(x)
+        x += residual
+
+        x = self.layer_norm(x)
+
+        return x
